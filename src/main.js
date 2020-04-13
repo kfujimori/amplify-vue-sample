7364e44ff2d5fc081d65c6524e40bf7b86edc98c
atiger7391 <atiger7391@gmail.com>
2020-04-14 00:09:52 +0900

    Add Lambda Logger.

--- src/main.js
+++ src/main.js
@@ -8,6 +8,20 @@ import aws_export from './aws-exports.js';
 Vue.config.productionTip = false
 Amplify.configure(aws_export)
 
+import Storage from '@aws-amplify/storage';
+import S3 from 'aws-sdk/clients/s3';
+
+const hackyCreateS3 = (options) => {
+  const {bucket, region, credentials} = options;
+  return new S3({
+    apiVersion: '2006-03-01',
+    params: { Bucket: bucket },
+    region: region,
+    credentials: credentials
+  });
+}
+Storage._createS3 = hackyCreateS3;
+
 new Vue({
   vuetify,
   router,
