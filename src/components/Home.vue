427701e72a89c33287017218c64e257bbfae9804
atiger7391 <atiger7391@gmail.com>
2020-04-13 23:47:48 +0900

    Modify Auth.signIn.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,5 +2,101 @@
   <v-container>
     <p>ホーム</p>
     <router-link to="about" >link to About</router-link>
+    <hr>
+    <v-btn @click="selectFile" color="primary" dark>SELECT A FILE !!</v-btn>
+    <input type="file" style="display:none" ref="input" @change="uploadSelectedFile()">
   </v-container>
 </template>
+
+<script>
+import{Auth, Storage} from 'aws-amplify';
+
+export default {
+  name: 'Home',
+  data: () => ({
+    loginid: "amplify-vue-sample-user",
+    loginpw: "keep1031"
+  }),
+  mounted: async function() {
+    this.login();
+  },
+  methods: {
+
+    /**
+     * ログイン認証
+     */
+    login() {
+      console.log("login.");
+
+      // const user = Auth.signIn({"username": this.loginid, "password": this.loginpw});
+      Auth.signIn({"username": this.loginid, "password": this.loginpw}).then((data) => {
+
+      // Auth.signIn(this.liginid, this.loginpw).then((data) => {
+        if(data.challengeName == "NEW_PASSWORD_REQUIRED") {
+          console.log("new password required.");
+          data.completeNewPasswordChallenge(this.loginpw, {}, 
+            {
+              onSuccess(result) {
+                console.log("onSuccess");
+                console.log(result);
+              },
+              onfailure(err) {
+                console.log("onfailure");
+                console.log(err);
+              }
+            }
+          );
+        }
+        console.log("login successfuly.");
+      }).catch((err) => {
+        console.log("login failed.");
+        console.log(err);
+      });
+    },
+
+    /**
+     * ファイル選択
+     */
+    selectFile() {
+      if(this.$refs.input != undefined) {
+        this.$refs.input.click();
+      }
+    },
+
+    /**
+     * ファイルアップロード
+     */
+    uploadSelectedFile() {
+      let file = this.$refs.input.files[0];
+      if(file == undefined) {
+        return;
+      }
+      console.log(file);
+
+      let dt = new Date();
+      let dirName = this.getDirString(dt);
+      let filePath = dirName + "/" + file.name;
+
+      Storage.put(filePath, file).then(result => {
+        console.log(result);
+      }).catch(err => {
+        console.log(err);
+      })
+    },
+
+    getDirString(date){
+      let random = date.getTime() + Math.floor(100000 * Math.random());
+      random = Math.random() * random;
+      random = Math.floor(random).toString(16);
+      return "" + 
+        ("00" + date.getUTCFullYear()).slice(-2) + 
+        ("00" + (date.getMonth() + 1)).slice(-2) + 
+        ("00" + date.getUTCDate()).slice(-2) + 
+        ("00" + date.getUTCHours()).slice(-2) + 
+        ("00" + date.getUTCMinutes()).slice(-2) + 
+        ("00" + date.getUTCSeconds()).slice(-2) + 
+        "-" + random;
+    },
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router';
+import Amplify from 'aws-amplify';
+import aws_export from './aws-exports.js';
 
 Vue.config.productionTip = false
+Amplify.configure(aws_export)
 
 new Vue({
   vuetify,
